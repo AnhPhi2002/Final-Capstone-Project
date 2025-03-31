0fb4ffd7da7def43cbc68cf18bc417c58b39fa82
AnhPhi2002 <phinhase161377@fpt.edu.vn>
2025-03-31 15:52:21 +0700

    fix:google client id login

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,9 @@ import { store } from "./lib/api/redux/store";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 
-const GOOGLE_CLIENT_ID = "401058314814-18103n9hp639mera1g1574h16ouojn4s.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_ID = "401058314814-18103n9hp639mera1g1574h16ouojn4s.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "304430377140-errqibvonam3v887s3msb402e2t99ch3.apps.googleusercontent.com";
+
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
